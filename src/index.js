c886cdd0f9893c3379f845e44728a23da8770577
Vladimir Drannikov <krowen@inbox.ru>
2021-05-12 17:15:33 +0600

    Added pictures

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import three from './images/3.jpg';
 import four from './images/4.jpg';
 import five from './images/5.jpg';
 import six from './images/6.jpg';
+import seven from './images/7.jpg';
+import eight from './images/8.jpg';
+import nine from './images/9.jpg';
+import ten from './images/10.jpg';
+import eleven from './images/11.jpg';
+import twelve from './images/12.jpg';
 import arrow from './images/icon-arrow.svg';
 
 import app from './app';
